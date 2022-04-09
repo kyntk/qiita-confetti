630c2f095175a8c7a9f515b72849b3b41e8ce661
kyntk <yuto.kaneko.1128@gmail.com>
2022-04-09 17:08:41 +0900

    Add effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,54 @@
+import { useEffect, useState } from 'react'
 import Confetti from 'react-confetti'
+import useWindowSize from 'react-use/lib/useWindowSize'
+
+const articlePageRegExp = (userName: string) =>
+  new RegExp(`^/${userName}/items/[\\w\\d]{20}$`, 'i')
+const likeRegExp = (userName: string) =>
+  new RegExp(`/${userName}/items/[\\w\\d]{20}/likers$`, 'i')
+
+type UserName = string | undefined
 
 export default () => {
-  const isBrowser = typeof window === 'undefined'
-  const width = isBrowser ? window.innerWidth : 2000
-  const height = isBrowser ? window.innerHeight : 1500
-  const lgtm = 0
+  const { width, height } = useWindowSize()
+  const [userName, setUserName] = useState<UserName>(undefined)
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const setUser = async () => {
+        console.log('setUser')
+
+        const { userName: strageUser } = await chrome.storage.local.get(
+          'userName'
+        )
+        console.log('strageUser', strageUser)
+        const userName: UserName = strageUser
+          ? strageUser
+          : window.prompt('あなたのユーザー名を入力してください', '')
+        chrome.storage.local.set({ userName })
+        setUserName(userName)
+      }
+      setUser()
+    }
+  }, [])
+
+  if (typeof window === 'undefined') return null
+  if (!userName?.length) return null
+  if (!articlePageRegExp(userName).test(window.location.pathname)) return null
+
+  const tag = Array.from(document.getElementsByTagName('a')).find((tag) =>
+    likeRegExp(userName).test(tag.href)
+  )
+
+  const lgtm = Number(tag?.innerText) || 0
   const num = Math.log(lgtm + 1) * 1000
 
-  return <Confetti width={width} height={height} numberOfPieces={num} />
+  return (
+    <Confetti
+      width={width}
+      height={height}
+      numberOfPieces={num}
+      style={{ position: 'fixed' }}
+    />
+  )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: './release/js',
+    lib: {
+      entry: path.resolve(__dirname, 'src/main.tsx'),
+      name: 'confetti',
+      formats: ['cjs'],
+    },
+  },
 })
