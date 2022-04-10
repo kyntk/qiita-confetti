2afb432cd62a39a62d7613c44295ef78dfd3ff2f
kyntk <yuto.kaneko.1128@gmail.com>
2022-04-10 15:12:48 +0900

    Revert "Create option page"
    This reverts commit e8b704b388426dab8327be1f42d0e7159d712f18.

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,16 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-const { resolve } = require('path')
+const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: './release/dist',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'main.html'),
-        option: resolve(__dirname, 'options.html'),
-      },
-      output: {
-        entryFileNames: '[name].js',
-      },
+    outDir: './release/js',
+    lib: {
+      entry: path.resolve(__dirname, 'src/main.tsx'),
+      name: 'confetti',
+      formats: ['cjs'],
     },
   },
 })
