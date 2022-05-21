8d32604a420028079d4088c2ec876915f6bfb3af
kyntk <yuto.kaneko.1128@gmail.com>
2022-05-22 01:05:57 +0900

    Add temporary disabling feature

--- packages/option/src/App.tsx
+++ packages/option/src/App.tsx
@@ -1,42 +1,30 @@
-import { useState } from 'react'
+import { ChangeEvent, useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [disabled, setDisabled] = useState(false)
+  useEffect(() => {
+    if (typeof chrome.storage === 'undefined') return
+    chrome.storage.local
+      .get('enabled')
+      .then(({ enabled }) => setDisabled(enabled === false))
+  }, [])
 
+  const handleCheck = (e: ChangeEvent<HTMLInputElement>) => {
+    setDisabled(e.currentTarget.checked)
+    chrome.storage?.local?.set({ enabled: !e.currentTarget.checked })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className='App'>
+      <label>
+        <input
+          type='checkbox'
+          name='enabled'
+          checked={disabled}
+          onChange={handleCheck}
+        />
+        記事を読みたい
+      </label>
     </div>
   )
 }
